d29d166b10621612ac38b55965c9291f357c3a27
KatyaQA <ekaterina.rus91@mail.ru>
2023-05-11 11:46:33 +0300

    first commit

---
+++ src/test/java/ru/netology/services/weekendServiceTest.java
@@ -0,0 +1,50 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+
+public class weekendServiceTest {
+    
+    @Test
+
+    public void showWeekend1() {
+        weekendService service = new weekendService();
+        
+        int expected = 3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+
+    public void showWeekend2() {
+        weekendService service = new weekendService();
+        
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        
+        Assertions.assertEquals(expected, actual);
+    }
+    
+    @ParameterizedTest
+    @CsvFileSource(resources = "/ru.netology.services/test_data.csv")
+    public void testWithCSVFile(int income, int expense, int threshold, int expected) {
+
+        weekendService service = new weekendService();
+
+
+        int actual = service.calculate(income,expense,threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
+  
+}
+
+
+
